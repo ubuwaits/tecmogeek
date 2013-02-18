ce0c9fbeae335abca6444a5865bd7541a86826d5
Chad Mazzola <chad.mazzola@gmail.com>
2013-02-17 23:48:49 -0500

    Footer. Work on style.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
   $(".toggle-rushing").click(function() {
     $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te rushing");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
     return false;
   });
 
   $(".toggle-receiving").click(function() {
     $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te receiving");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
     return false;
   });
 
   $(".toggle-kick-return").click(function() {
     $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te kick-return");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
     return false;
   });
 
   $(".toggle-punt-return").click(function() {
     $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te punt-return");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
     return false;
   });
 });
