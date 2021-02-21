208d16fd917f184a156f03015f6c7503af280155
Chad Mazzola <223076+ubuwaits@users.noreply.github.com>
2021-02-21 13:18:26 +0100

    Huge cleanup and refactor

--- Rakefile
+++ Rakefile
@@ -1,10 +1,10 @@
-desc 'Start Jekyll server and watch Sass/Bourbon files'
+desc 'Start Jekyll server and watch Sass files'
 task :s do
-  puts "Starting the Jekyll server and watching Sass files."
+  puts 'Starting the Jekyll server and watching Sass files.'
   jekyllPid = Process.spawn('jekyll s --livereload')
-  sassPid = Process.spawn('sass --watch stylesheets/scss:stylesheets --style compressed')
+  sassPid = Process.spawn('sass --watch css/scss:css --style compressed')
 
-  trap("INT") {
+  trap('INT') {
     [jekyllPid, sassPid].each { |pid| Process.kill(9, pid) rescue Errno::ESRCH }
     exit 0
   }
---
+++ js/application.js
@@ -0,0 +1,167 @@
+$(document).ready(function() {
+  $('.player-listing').tsort('.rushing-ranking', {order: 'asc'});
+
+  // Sort by player abilities
+  // ***************************************************************************
+  $(".player-ranking a").click(function() {
+    $('.player-listing').tsort('.player-ranking', {order: 'asc'});
+    return false;
+  });
+
+  $(".player-rating-label a").click(function() {
+    $('.player-listing').tsort('.player-rating', {order: 'desc'});
+    return false;
+  });
+
+  $(".running-speed a").click(function() {
+    $('.player-listing').tsort('.running-speed', {order: 'desc'});
+    return false;
+  });
+
+  $(".maximum-speed a").click(function() {
+    $('.player-listing').tsort('.maximum-speed', {order: 'desc'});
+    return false;
+  });
+
+  $(".passing-speed a").click(function() {
+    $('.player-listing').tsort('.passing-speed', {order: 'desc'});
+    return false;
+  });
+
+  $(".pass-control a").click(function() {
+    $('.player-listing').tsort('.pass-control', {order: 'desc'});
+    return false;
+  });
+
+  $(".avoid-pass-block a").click(function() {
+    $('.player-listing').tsort('.avoid-pass-block', {order: 'desc'});
+    return false;
+  });
+
+  $(".hitting-power a").click(function() {
+    $('.player-listing').tsort('.hitting-power', {order: 'desc'});
+    return false;
+  });
+
+  $(".ball-control a").click(function() {
+    $('.player-listing').tsort('.ball-control', {order: 'desc'});
+    return false;
+  });
+
+  $(".receptions a").click(function() {
+    $('.player-listing').tsort('.receptions', {order: 'desc'});
+    return false;
+  });
+
+  $(".rushing-power a").click(function() {
+    $('.player-listing').tsort('.rushing-power', {order: 'desc'});
+    return false;
+  });
+
+  $(".pass-interceptions a").click(function() {
+    $('.player-listing').tsort('.pass-interceptions', {order: 'desc'});
+    return false;
+  });
+
+  $(".kicking-ability a").click(function() {
+    $('.player-listing').tsort('.kicking-ability', {order: 'desc'});
+    return false;
+  });
+
+  $(".avoid-kick-block a").click(function() {
+    $('.player-listing').tsort('.avoid-kick-block', {order: 'desc'});
+    return false;
+  });
+
+  // Show/hide WR & TE on rushers page
+  // ***************************************************************************
+  $(".rusher-toggle .show-all").click(function() {
+    $(".player-WR1, .player-WR2, .player-WR3, .player-WR4, .player-WR5, .player-WR6, .player-TE1, .player-TE2").show();
+    $(".only-rb").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  $(".rusher-toggle .only-rb").click(function() {
+    $(".player-WR1, .player-WR2, .player-WR3, .player-WR4, .player-WR5, .player-WR6, .player-TE1, .player-TE2").hide();
+    $(".show-all").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  // Show/hide RB on receivers page
+  // ***************************************************************************
+  $(".receiver-toggle .show-all").click(function() {
+    $(".player-RB1, .player-RB2, .player-RB3, .player-RB4").show();
+    $(".only-wr").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  $(".receiver-toggle .only-wr").click(function() {
+    $(".player-RB1, .player-RB2, .player-RB3, .player-RB4").hide();
+    $(".show-all").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  // Toggle between position ratings on team page
+  // ***************************************************************************
+  $(".toggle-default").click(function() {
+    $('.player-listing').tsort({data: 'number'});
+    $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te default");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  $(".toggle-rushing").click(function() {
+    $('.player-listing').tsort('.rushing-ranking', {order: 'asc'});
+    $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te rushing");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  $(".toggle-receiving").click(function() {
+    $('.player-listing').tsort('.receiving-ranking', {order: 'asc'});
+    $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te receiving");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  $(".toggle-kick-return").click(function() {
+    $('.player-listing').tsort('.kick-return-ranking', {order: 'asc'});
+    $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te kick-return");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  $(".toggle-punt-return").click(function() {
+    $('.player-listing').tsort('.punt-return-ranking', {order: 'asc'});
+    $(".players.rb-wr-te").removeClass().addClass("players rb-wr-te punt-return");
+    $(".skill-toggles li a").removeClass("active");
+    $(this).addClass("active");
+    return false;
+  });
+
+  $("nav li.nav-teams > a").click(function() {
+    $("nav li.nav-players > a").removeClass("active");
+    $("nav li.nav-players > ol").hide();
+    $("nav li.nav-teams > a").toggleClass("active");
+    $("nav li.nav-teams > ol").toggle();
+    return false;
+  });
+
+  $("nav li.nav-players > a").click(function() {
+    $("nav li.nav-teams > a").removeClass("active");
+    $("nav li.nav-teams > ol").hide();
+    $("nav li.nav-players > a").toggleClass("active");
+    $("nav li.nav-players > ol").toggle();
+    return false;
+  });
+
+  $("a").tooltip();
+});
